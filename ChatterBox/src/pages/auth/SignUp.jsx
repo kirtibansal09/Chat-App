aaf68600557c6f49d517ff4ca7c62279f2966330
Rahul Vashishtha <68159064+RahulVashishtha1@users.noreply.github.com>
2025-03-16 13:55:05 +0530

    Handled Signup Form

--- ChatterBox/src/pages/auth/SignUp.jsx
+++ ChatterBox/src/pages/auth/SignUp.jsx
@@ -3,7 +3,30 @@ import Logo from "../../components/Logo";
 import SignupIllustration from "../../assets/images/auth/signup.svg";
 import { EnvelopeSimple, Lock, User } from "@phosphor-icons/react";
 // import { RegisterUser } from "../../redux/slices/auth";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { RegisterUser } from "../../redux/slices/auth";
+
+// Validation Schema
+const schema = yup.object().shape({
+  name: yup.string().required("Name is required"),
+
+  email: yup
+    .string()
+    .email("Please enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "Password must match")
+    .required("Please confirm your password"),
+});
 
 function Signup() {
   const dispatch = useDispatch();
@@ -12,6 +35,28 @@ function Signup() {
   // const submitForm = () => {
   //   dispatch(RegisterUser(formData));
   // }
+
+  const { isLoading } = useSelector((state) => state.auth);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data, "Form Data: Signup");
+
+    dispatch(RegisterUser(data));
+  };
 
   return (
     <div className="border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark h-full">
@@ -24,7 +69,7 @@ function Signup() {
             </Link>
 
             <p>
-             Join ChatterBox & experience the moderm way to connect with people
+              Join ChatterBox & experience the moderm way to connect with people
             </p>
 
             <span className="mt-15 inline-block">
@@ -45,8 +90,8 @@ function Signup() {
               Sign Up to ChatterBox
             </h2>
 
-            <form action="">
-            <div className="mb-4">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="mb-4">
                 <label
                   htmlFor=""
                   className="mb-2.5 block font-medium text-black dark:text-white"
@@ -55,13 +100,26 @@ function Signup() {
                 </label>
 
                 <div className="relative">
-                  <input type="text" placeholder="Enter your full name" className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-slate-500 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary " />
+                  <input
+                    type="text"
+                    {...register("name")}
+                    placeholder="Enter your full name"
+                    className={`w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-slate-500 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary ${
+                      errors.name
+                        ? "border-red focus:border-red"
+                        : "border-stroke"
+                    }`}
+                  />
 
                   <span className="absolute right-4 top-4">
                     <User size={24} />
                   </span>
                 </div>
+                {errors.name && (
+                  <p className="text-red text-sm">{errors.name.message}</p>
+                )}
               </div>
+
               <div className="mb-4">
                 <label
                   htmlFor=""
@@ -71,12 +129,24 @@ function Signup() {
                 </label>
 
                 <div className="relative">
-                  <input type="email" placeholder="Enter your email" className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-slate-500 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary " />
+                  <input
+                    type="email"
+                    {...register("email")}
+                    placeholder="Enter your email"
+                    className={`w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-slate-500 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary ${
+                      errors.email
+                        ? "border-red focus:border-red"
+                        : "border-stroke"
+                    }`}
+                  />
 
                   <span className="absolute right-4 top-4">
                     <EnvelopeSimple size={24} />
                   </span>
                 </div>
+                {errors.email && (
+                  <p className="text-red text-sm">{errors.email.message}</p>
+                )}
               </div>
 
               <div className="mb-6">
@@ -88,14 +158,24 @@ function Signup() {
                 </label>
 
                 <div className="relative">
-                  <input type="password"
+                  <input
+                    type="password"
+                    {...register("password")}
                     placeholder="Enter your password"
-                    className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-slate-500 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary " />
+                    className={`w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-slate-500 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary ${
+                      errors.password
+                        ? "border-red focus:border-red"
+                        : "border-stroke"
+                    }`}
+                  />
 
                   <span className="absolute right-4 top-4">
                     <Lock size={24} />
                   </span>
                 </div>
+                {errors.password && (
+                  <p className="text-red text-sm">{errors.password.message}</p>
+                )}
               </div>
 
               <div className="mb-6">
@@ -107,27 +187,47 @@ function Signup() {
                 </label>
 
                 <div className="relative">
-                  <input type="password"
+                  <input
+                    type="password"
+                    {...register("confirmPassword")}
                     placeholder="Re-type your password"
-                    className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-slate-500 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary " />
+                    className={`w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-slate-500 focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary ${
+                      errors.confirmPassword
+                        ? "border-red focus:border-red"
+                        : "border-stroke"
+                    }`}
+                  />
 
                   <span className="absolute right-4 top-4">
                     <Lock size={24} />
                   </span>
                 </div>
+                {errors.confirmPassword && (
+                  <p className="text-red text-sm">
+                    {errors.confirmPassword.message}
+                  </p>
+                )}
               </div>
 
               <div className="mb-5">
-                <input onClick={()=>{
-                  navigate("/auth/verify")
-                }} type="submit" value="Create account" className="w-full cursor-pointer border border-primary bg-primary p-4 text-white rounded-lg transition hover:bg-opacity-90 " />
+                <button
+                  type="submit"
+                  disabled={isSubmitting || isLoading}
+                  className="w-full cursor-pointer border border-primary bg-primary p-4 text-white rounded-lg transition hover:bg-opacity-90 "
+                >
+                  {isSubmitting || isLoading
+                    ? "Submitting..."
+                    : "Create Account"}
+                </button>
               </div>
 
-              <button onClick={()=>{
-                navigate("/dashboard")
-              }} className="flex flex-row w-full items-center justify-center gap-3.5 border border-stroke bg-gray p-4 rounded-lg hover:bg-opacity-50 dark:border-strokedark dark:bg-meta-4 dark:hover:bg-opacity-50">
+              {/* <button
+                onClick={() => {
+                  navigate("/dashboard");
+                }}
+                className="flex flex-row w-full items-center justify-center gap-3.5 border border-stroke bg-gray p-4 rounded-lg hover:bg-opacity-50 dark:border-strokedark dark:bg-meta-4 dark:hover:bg-opacity-50"
+              >
                 <span>
-
                   <svg
                     width="20"
                     height="20"
@@ -159,17 +259,13 @@ function Signup() {
                       </clipPath>
                     </defs>
                   </svg>
-
-    
-
                 </span>
-
                 Sign up with Google
-              </button>
+              </button> */}
 
               <div className="mt-6 text-center">
                 <p>
-                  Already have an account? {" "}
+                  Already have an account?{" "}
                   <Link to="/auth/login" className="text-primary">
                     Sign in
                   </Link>
