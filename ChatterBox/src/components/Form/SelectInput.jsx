50450402ce7c91459d58340428e404ca77f81f1b
kirtibansal09 <kirtibansalbikaner@gmail.com>
2025-06-08 16:34:37 +0530

    Profile updating done

--- ChatterBox/src/components/Form/SelectInput.jsx
+++ ChatterBox/src/components/Form/SelectInput.jsx
@@ -1,13 +1,7 @@
 import { CaretDown, Globe } from "@phosphor-icons/react";
-import React, { useState } from "react";
+import React from "react";
 
-export default function SelectInput() {
-  const [selectedOption, setSelectedOption] = useState("");
-  const [isOptionSelected, setIsOptionSelected] = useState(false);
-
-  const changeTextColor = () => {
-    setIsOptionSelected(true);
-  };
+export default function SelectInput({ selected, onChange }) {
   return (
     <div>
       <label className="mb-3 block text-black dark:text-white">
@@ -20,13 +14,10 @@ export default function SelectInput() {
         </span>
 
         <select
-          value={selectedOption}
-          onChange={(e) => {
-            setSelectedOption(e.target.value);
-            changeTextColor();
-          }}
+          value={selected}
+          onChange={(e) => onChange(e.target.value)}
           className={`relative z-20 w-full appearance-none rounded border border-stroke bg-transparent px-12 py-3 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input ${
-            isOptionSelected ? "text-black dark:text-white" : ""
+            selected ? "text-black dark:text-white" : ""
           }`}
         >
           <option value="" disabled className="text-body dark:text-bodydark">
